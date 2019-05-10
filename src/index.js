71dd00fae66e986bac855213a69aca2b2880a591
Madeline Morton <madelinemorton614@gmail.com>
2019-05-10 17:03:28 -0500

    index js setup

--- src/index.js
+++ src/index.js
@@ -3,6 +3,21 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
 import registerServiceWorker from './registerServiceWorker';
+import logger from 'redux-logger';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Redux stuff import
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+
+
+// create store and add middleware
+const storeInstance = createStore(
+    combineReducers({
+        
+    }),
+    applyMiddleware(logger),
+);
+
+
+ReactDOM.render(<Provider store={storeInstance}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
